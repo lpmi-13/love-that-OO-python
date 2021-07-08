20b0904b22dc2e3ead9a010183a3b5c19f2f48f2
Adam Leskis <leskis@gmail.com>
2021-07-08 23:37:01 +0100

    suggested updates

--- Part2Draft.py
+++ Part2Draft.py
@@ -1,15 +1,20 @@
 #Defines current time
 currentHour = 7
 
+buying_price_of_robots = 100
+selling_price_of_robots = 350
+parts_per_robot = 5
+price_of_parts = 10
+
 #Class for a factory that makes robots
 class RobotFactory:
 
+	money = 1000
 	#Constructor for the class. takes in parameters robots and parts.
-	def __init__(self, robots, parts, money = 20):
+	def __init__(self, robots, parts):
 
 		self.robots = robots
 		self.parts = parts
-		self.money = money
 
 	#Accessor for robots
 	def getRobots(self):
@@ -33,37 +38,51 @@ class RobotFactory:
 
 	#Mutator for robot and parts
 	def createRobot(self):
+		if self.money <= buying_price_of_robots:
+			return "can't create any more robots, we don't have enough money"
 
-		self.setParts(self.parts - 5)
+		self.setParts(self.parts - parts_per_robot)
 		self.setRobots(self.robots + 1)
+		self.setMoney(self.money - buying_price_of_robots)
 
 	#Mutator for money
-	def setMoney(self, newMoney):
+	def setMoney(self, amount):
 
-		if newMoney < 0:
-			newMoney = 0
-
-			self.money = newMoney
+		self.money = amount
 
 	#Function defining buying of parts
 	def buyParts(self):
+		if self.money <= price_of_parts:
+			return "can't buy any parts, we don't have enough money"
 			
 		#Function defining selling of robots
-		self.setPart
-		self.setMoney(self.getMoney) - part.getPrice
+
+		self.setParts(self.parts + parts_per_robot)
+		self.setMoney(self.money - price_of_parts)
 
 	#
 	def sellRobot(self):
+		if self.robots == 0:
+			return "we're out of robots, we can't sell any more"
 			
-		#Set money to be equal to current amount minus price of robot
-		self.setRobot
-		self.setMoney
+		self.setRobots(self.robots - 1)
+		self.setMoney(self.money + selling_price_of_robots)
+
 	#Add a function called printVariables here that prints how many parts and robots are in the factory as well as the current amount of money and the current time
+	def printVariables(self):
+		global currentHour
+		print(f'there are {self.parts} parts and {self.robots} robots')
+		print(f'it is also currently hour {currentHour} and we have {self.money} amount of money')
 
 #Function sets passing of time when called
 def passTime():
 
-	#
+	# increments the hour by 1, and resets to 0 after 23
+	global currentHour
+	if currentHour >= 23:
+		currentHour = 0
+	else:
+		currentHour += 1
 
 #
 factory1 = RobotFactory(2, 100)
@@ -71,11 +90,11 @@ factory1 = RobotFactory(2, 100)
 #
 #factory1.createRobot()
 #factory1.buyParts()
-factory1.buyparts(part1)
-factory1.buyparts(part2)
+factory1.buyParts()
+factory1.buyParts()
 #factory1.sellRobot()
-factory1.sellRobot(robot1)
-factory1.sellRobot(robot2)
+factory1.sellRobot()
+factory1.sellRobot()
 #passTime()
 
 #printVariables()
